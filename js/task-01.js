cdc277499a4f734f9480239766b27a65ceba1c63
UA-DmitriyS <tavluy86@gmail.com>
2023-02-25 15:44:09 +0200

    task1+

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,10 @@
-const x = 10;
+const categoryEl = document.querySelectorAll("li.item");
+console.log(`Number of categories : ${categoryEl.length}`);
+
+categoryEl.forEach((element) => {
+  const titleEl = element.querySelector(`h2`);
+  console.log(`Categoty: ${titleEl.textContent}`);
+
+  const listEl = element.querySelectorAll("li");
+  console.log(`Elements : ${listEl.length}`);
+});
