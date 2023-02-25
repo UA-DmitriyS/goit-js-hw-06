598c7ce6cbc04007a638b905f399f7b40c50a5a7
UA-DmitriyS <tavluy86@gmail.com>
2023-02-25 18:36:28 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const colorSpanEl = document.querySelector(".color");
+const btnColorEl = document.querySelector(".change-color");
+
+function chBgCol() {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  colorSpanEl.textContent = randomColor;
+}
+btnColorEl.addEventListener("click", chBgCol);
