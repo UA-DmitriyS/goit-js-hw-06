3a97ded2fa742a9f478336654c1b2e0d3d6d3370
UA-DmitriyS <tavluy86@gmail.com>
2023-02-25 18:19:34 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const emailEl = document.querySelector("[type=email]");
+  const passwordEl = document.querySelector("[type=password]");
+
+  if (emailEl.value === " " || passwordEl.value === "") {
+    alert("Заповніть всі поля");
+    return;
+  }
+  const object = {
+    email: emailEl.value,
+    password: passwordEl.value,
+  };
+  console.log(object);
+  formEl.reset();
+});
